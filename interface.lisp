379662e73799e07568f5194ab33e6f30ae5d3fdb
Panji Kusuma <epanji@gmail.com>
2019-06-14 21:34:48 +0700

    Add interface.lisp

---
+++ interface.lisp
@@ -0,0 +1,11 @@
+;;;; guetor/interface.lisp
+
+(uiop:define-package :guetor/interface (:nicknames :guetor)
+  (:use :cl)
+  (:mix :guetor/content)
+  (:export #:*selector*
+           #:*mode*
+           #:document
+           #:available-elements
+           #:element-selector
+           #:selector))
