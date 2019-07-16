0bbd668fedb879ee6c17a3726b89caf135fa073f
Panji Kusuma <epanji@gmail.com>
2019-07-16 13:07:11 +0700

    Change for current interface

--- interface.lisp
+++ interface.lisp
@@ -2,12 +2,27 @@
 
 (uiop:define-package :guetor/interface (:nicknames :guetor)
   (:use :cl)
-  (:mix :guetor/content :guetor/content-markless)
-  (:export #:*selector*
-           #:*mode*
-           #:document
-           #:available-elements
-           #:element-selector
-           #:selector
-           #:contents-wrapper
-           #:output-markless))
+  (:mix
+   :guetor/content
+   :guetor/title
+   :guetor/output
+   :guetor/content-markless
+   :guetor/content-markdown)
+  (:export
+   ;; guetor/content
+   #:*mode*
+   #:*content*
+   #:*document*
+   #:*selector*
+   #:document
+   #:available-elements
+   #:element-selector
+   #:selector
+   #:contents-wrapper
+   ;; guetor/title
+   #:has-title-p
+   #:title
+   ;; guetor/content-markless
+   #:output-markless
+   ;; guetor/content-markdown
+   #:output-markdown))
